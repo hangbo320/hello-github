070bb33fd00629d5d78631cccb184ed3ac20a84c
hangbo <hangbo@hangbodeMac.local>
2014-06-20 20:28:35 +0800

    testing !!!

---
+++ testing/testing/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testing
+//
+//  Created by hangbo on 14-6-20.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ testing/testing/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  testing
+//
+//  Created by hangbo on 14-6-20.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    NSLog(@"i am trying!!!");
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
